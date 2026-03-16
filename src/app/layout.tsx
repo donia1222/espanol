54e4a43fc2915a7cc0fdf9d1163d8c1ad134cc3e
lwev <106478077+lwev@users.noreply.github.com>
2026-03-16 14:41:46 +0100

    ,mejrasadmintes2“

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Cormorant_Garamond, Outfit } from "next/font/google";
 import "./globals.css";
 
+
 const cormorant = Cormorant_Garamond({
   variable: "--font-serif",
   subsets: ["latin"],
